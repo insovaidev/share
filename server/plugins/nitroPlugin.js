25b1b5c5ee91ea0c570c4542f90e6537415ace13
insovai <insovaidev@gmail.com>
2024-09-13 15:18:58 +0700

    'justpush'

--- server/plugins/nitroPlugin.js
+++ server/plugins/nitroPlugin.js
@@ -5,19 +5,19 @@ export default defineNitroPlugin((nitroApp) => {
     const isMobile = /mobile/i.test(userAgent);
 
       
-   console.log('test nitroPlugin', html.head[0])
+   // console.log('test nitroPlugin', html.head[0])
 
-    html.head.push(`
-      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
-      <meta name="title", content="TMy Amazing Site">
-      <meta name="description", content="This is my amazing site, let me tell you all about it.">
-      <meta property="og:url" content="https://stately-squirrel-915b70.netlify.app/tweeter">
-      <meta property="og:image", content="https://images.khmer24.co/24-04-23/111248-business-house-for-rent-on-main-road-way-to-angkor-wat-1713857765-79938271-b.jpg">
-      <meta name="twitter:card", content="summary">
-      <meta name="twitter:title", content="My Amazing Site" >
-      <meta name="twitter:description", content="This is my amazing site, let me tell you all about it" >
-      <meta name="twitter:image", content="https://images.khmer24.co/24-04-23/111248-business-house-for-rent-on-main-road-way-to-angkor-wat-1713857765-79938271-b.jpg" >
-    `);
+   //  html.head.push(`
+   //    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
+   //    <meta name="title", content="TMy Amazing Site">
+   //    <meta name="description", content="This is my amazing site, let me tell you all about it.">
+   //    <meta property="og:url" content="https://stately-squirrel-915b70.netlify.app/tweeter">
+   //    <meta property="og:image", content="https://images.khmer24.co/24-04-23/111248-business-house-for-rent-on-main-road-way-to-angkor-wat-1713857765-79938271-b.jpg">
+   //    <meta name="twitter:card", content="summary">
+   //    <meta name="twitter:title", content="My Amazing Site" >
+   //    <meta name="twitter:description", content="This is my amazing site, let me tell you all about it" >
+   //    <meta name="twitter:image", content="https://images.khmer24.co/24-04-23/111248-business-house-for-rent-on-main-road-way-to-angkor-wat-1713857765-79938271-b.jpg" >
+   //  `);
 
    //  if (!isMobile) {
    //    // Desktop: Set viewport and background style
@@ -34,13 +34,15 @@ export default defineNitroPlugin((nitroApp) => {
    //    html.head.push(`
    //      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=0.0, maximum-scale=0.0">
    //    `);
-      html.head.push(`
-        <style>
-          body {
-            background-color: #000000; /* Dark background for mobile */
-          }
-        </style>
-      `);
+
+
+      // html.head.push(`
+      //   <style>
+      //     body {
+      //       background-color: #000000; /* Dark background for mobile */
+      //     }
+      //   </style>
+      // `);
    //  }
   });
 });
