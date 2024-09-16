bb3d94f676d6a58443cad28493b610eceba0a243
insovai <insovaidev@gmail.com>
2024-09-16 11:12:59 +0700

    'justpush'

--- pages/new-test-share.vue
+++ pages/new-test-share.vue
@@ -9,7 +9,7 @@ useSeoMeta({
   ogTitle: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
   ogDescription: 'This is my amazing site, let me tell you all about it. Tweeter',
   ogImage: 'https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg',
-  ogUrl: 'https://test-share-seo.netlify.app/test-share', 
+  ogUrl: 'https://test-share-seo.netlify.app/new-test-share', 
   twitterTitle: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
   twitterDescription: 'This is my amazing site, let me tell you all about it Tweeter just edit',
   twitterImage: 'https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg',
