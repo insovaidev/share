bf843f1d4a90428143fc38111b70cbb83e1e2355
insovai <insovaidev@gmail.com>
2024-09-16 11:23:33 +0700

    'justpush'

--- pages/new-test-share.vue
+++ pages/new-test-share.vue
@@ -1,15 +1,17 @@
 <template>
    <div>
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="Sammy the Shark Edit" />
-    <meta property="og:description" content="Senior Selachimorpha at DigitalOcean Edit" />
-    <meta property="og:url" content="https://test-share-seo.netlify.app/new-test-share" />
-    <meta property="og:image" content="https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="https://test-share-seo.netlify.app/new-test-share" />
-    <meta name="twitter:title" content="Sammy the Shark Edit" />
-    <meta name="twitter:description" content="Senior Selachimorpha at DigitalOcean Edit" />
-    <meta name="twitter:image" content="https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg" />
+    <Head>
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="Sammy the Shark Edit" />
+      <meta property="og:description" content="Senior Selachimorpha at DigitalOcean Edit" />
+      <meta property="og:url" content="https://test-share-seo.netlify.app/new-test-share" />
+      <meta property="og:image" content="https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="https://test-share-seo.netlify.app/new-test-share" />
+      <meta name="twitter:title" content="Sammy the Shark Edit" />
+      <meta name="twitter:description" content="Senior Selachimorpha at DigitalOcean Edit" />
+      <meta name="twitter:image" content="https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg" />
+    </Head>
    </div>
 </template>
 
