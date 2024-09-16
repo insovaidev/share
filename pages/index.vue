bdc8455c0886ab6ff920799138e0598efceb8f59
insovai <insovaidev@gmail.com>
2024-09-16 09:19:40 +0700

    'justpush'

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@ const getPosts = async () => {
 };
 
 const toDetail = (title, id) => {
-  router.push({ name: "post-adid-id", params: { id: id } });
+  router.push({ name: "test-share-adid-id", params: { id: id } });
 };
 
 const share = async (post) => {
