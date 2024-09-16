726984b64cfe715d5c7c762f223651b08f78a25c
insovai <insovaidev@gmail.com>
2024-09-16 11:20:59 +0700

    'justpush'

--- pages/new-test-share.vue
+++ pages/new-test-share.vue
@@ -1,22 +1,33 @@
 <template>
-   <p>Lorem</p>
+   <div>
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Sammy the Shark Edit" />
+    <meta property="og:description" content="Senior Selachimorpha at DigitalOcean Edit" />
+    <meta property="og:url" content="https://test-share-seo.netlify.app/new-test-share" />
+    <meta property="og:image" content="https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="https://test-share-seo.netlify.app/new-test-share" />
+    <meta name="twitter:title" content="Sammy the Shark Edit" />
+    <meta name="twitter:description" content="Senior Selachimorpha at DigitalOcean Edit" />
+    <meta name="twitter:image" content="https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg" />
+   </div>
 </template>
 
 <script setup>
 
-useSeoMeta({
-  title: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
-  ogTitle: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
-  ogDescription: 'This is my amazing site, let me tell you all about it. Tweeter',
-  ogImage: 'https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg',
-  ogUrl: 'https://test-share-seo.netlify.app/new-test-share', 
-  twitterTitle: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
-  twitterDescription: 'This is my amazing site, let me tell you all about it Tweeter just edit',
-  twitterImage: 'https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg',
-  twitterCard: 'summary',
-  ogImageWidth: 1200,  
-  ogImageHeight: 630
-})
+// useSeoMeta({
+//   title: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
+//   ogTitle: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
+//   ogDescription: 'This is my amazing site, let me tell you all about it. Tweeter',
+//   ogImage: 'https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg',
+//   ogUrl: 'https://test-share-seo.netlify.app/new-test-share', 
+//   twitterTitle: 'My Amazing Site Tweeter just edit' + ' ' + new Date().getTime(),
+//   twitterDescription: 'This is my amazing site, let me tell you all about it Tweeter just edit',
+//   twitterImage: 'https://images.khmer24.co/24-09-04/scoopy-i-015--775039172541923673824722-b.jpg',
+//   twitterCard: 'summary',
+//   ogImageWidth: 1200,  
+//   ogImageHeight: 630
+// })
 
 </script>
 
